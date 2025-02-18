fef12e3bc7b694f4366560f846ceccf24a6aa757
shishirdubey24 <shishirdubey221@gmail.com>
2025-02-18 19:43:11 +0530

    Added BAckground image

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,15 @@
+import Header from "./Header"
 
 const Login = () => {
   return (
     <div>
       Login hre!
+      <Header/>
+      <div className="Background-img">
+
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg" />
+      </div>
+       
     </div>
   )
 }
