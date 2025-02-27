5cb96451695fcbbf3d33f2fb24c92727a6c7d725
shishirdubey24 <shishirdubey221@gmail.com>
2025-02-27 19:19:29 +0530

    Added FireBAse AUTH

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,52 @@
 import { useState,useRef } from "react"
 import Header from "./Header"
 import ValidationForm from "../utils/ValidationForm";
+import {  createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
+import { auth} from "../utils/Firebase";
 const Login = () => {
   const[isSignedIn,setSignedIn]=useState(true);
-
+  const [errormsg,seterrormsg] = useState(null);
  const handleSignup=()=>{
   setSignedIn(!isSignedIn)
  }
 const handleBtnClick=()=>{
- const msz=ValidationForm(email.current.value, password.current.value);
- console.log(msz);
+ const message=ValidationForm(email.current.value, password.current.value);
+ seterrormsg(message); 
+ if(message) return;
+
+ if(!isSignedIn){
+  //SIgn-up Logic
+ 
+  createUserWithEmailAndPassword(auth, 
+    email.current.value,
+     password.current.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      seterrormsg(errorMessage+errorCode);
+      // ..
+    });
+ }
+ else{
+  signInWithEmailAndPassword(auth,email.current.value,
+    password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+   seterrormsg(errorMessage+"\n\n"+errorCode)
+  });
+ }
 }
 const email=useRef(null);
 const password=useRef(null);
@@ -22,12 +59,18 @@ const password=useRef(null);
        
    <form  onSubmit={(e)=>e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-40 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className="font-bold text-2xl">{isSignedIn?"SIgnIn":"SignUp"}</h1>
+
+
         {!isSignedIn&& ( <input  type="Name" placeholder="Full Name" className="p-2 my-2 w-full bg-gray-700"/>) }
+
+
         {!isSignedIn &&(<input type="Phone Number" placeholder="Number" className="p-2 my-2 w-full bg-gray-700"/>)}
+
+
           <input ref={email} type="text" placeholder="Email Address" className="p-2 my-2 w-full  bg-gray-700"  />
           <input ref={password} type="password" placeholder="Enter password" className="p-2 my-2 w-full bg-gray-700"/>
 
-     
+<p className="text-red-800 font-bold text-lg py-2.5" > {errormsg }</p>     
  <button className="p-2 my-4 bg-red-500 w-full rounded-lg " onClick={handleBtnClick}>{isSignedIn?"SignIn":"SingUp"}</button>
         
         <p className="py-4 " onClick={handleSignup}>{isSignedIn?"New here SIgnup NOw":"Already regestered Lets SinIn Now"}</p> 
