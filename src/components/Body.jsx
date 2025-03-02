1bccbd99fb8f431d10cdff8eeccb575a0712b122
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-02 20:39:44 +0530

    Improved file structer

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,46 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./Login"
 import Browse from "./Browse"
-const Body = () => {
- 
- const router=createBrowserRouter([
-    {
-        path :"/",element: <Login/>
-    },
-    {
-        path : "/Browse", element: <Browse/>
-    },
-   
- ])
- 
-    return (
-    <div>
-      <RouterProvider router={router}/>
-    </div>
-  )
-}
+import { useEffect } from "react"
+import {  onAuthStateChanged } from "firebase/auth"
+import{auth} from "../utils/Firebase"
+import { useDispatch } from "react-redux"
+import { addUser, removeUser } from "../utils/userSlice"
 
-export default Body
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+]);
+
+const Body = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        console.log("User is signed in:", user);
+        dispatch(addUser({ uid, email, displayName: displayName || "User" }));
+      } else {
+        console.log("User is signed out");
+        dispatch(removeUser());
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup function
+  }, [dispatch]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default Body;
