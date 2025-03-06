29e493479c06b7c623f525bfd06f9893a24a7523
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-06 20:17:09 +0530

    Commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import ValidationForm from "../utils/ValidationForm";
 import {  createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
 import { auth} from "../utils/Firebase";
 import { useNavigate } from "react-router-dom";
+import { Logo } from "../utils/Constant";
 
 const Login = () => {
   const[isSignedIn,setSignedIn]=useState(true);
@@ -63,7 +64,7 @@ const password=useRef(null);
     <div >
       <Header/>
       <div className="absolute ">
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg" />
+      <img src= {Logo} />
       </div>
        
    <form  onSubmit={(e)=>e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-40 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
@@ -82,7 +83,7 @@ const password=useRef(null);
 <p className="text-red-800 font-bold text-lg py-2.5" > {errormsg }</p>     
  <button className="p-2 my-4 bg-red-500 w-full rounded-lg " onClick={handleBtnClick}>{isSignedIn?"SignIn":"SingUp"}</button>
         
-        <p className="py-4 " onClick={handleSignup}>{isSignedIn?"New here SIgnup NOw":"Already regestered Lets SinIn Now"}</p> 
+        <p className="py-4 " onClick={handleSignup}>{isSignedIn?"--> New here SIgnup Now <--":"-->Already regestered Lets SinIn Now <--"}</p> 
         </form>
        
 
