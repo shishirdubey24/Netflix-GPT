d320d2680019790118ccb923981302917cbafb5b
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-06 20:17:44 +0530

    Commit

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -6,6 +6,7 @@ const userSlice = createSlice({
   initialState: null,
   reducers: {
     addUser: (state, action) => {
+      console.log("addUser is called");
       return action.payload;
     },
     removeUser: (state, action) => {
