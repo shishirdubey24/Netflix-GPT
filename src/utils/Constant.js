435ed3fb04a409c35ce7ca718ce1ab49a3caa640
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-06 20:18:24 +0530

    Commit

---
+++ src/utils/Constant.js
@@ -0,0 +1,5 @@
+export const Logo =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg";
+
+export const Image =
+  "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
