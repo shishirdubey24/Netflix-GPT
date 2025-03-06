e33b9280ebcecadd3bfc147d58c19bd135b9310b
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-06 20:16:46 +0530

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import {  signOut } from "firebase/auth";
 import { auth} from "../utils/Firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Image } from "../utils/Constant";
 const Header = () => {
   const navigate=useNavigate();
+  const User=useSelector((store)=>store.user)
   const handleSignout=()=>{
     console.log("SignOut Is Clicked");
     signOut(auth)
@@ -20,13 +23,13 @@ const Header = () => {
    
     <img className="w-44"
       
-     src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" />
+     src= {Image} />
 
-     <div className="flex p-2">
+   {User && <div className="flex p-2">
      <img className="w-12 h-12"
       src="/public/Images/NetFlix_user-icon.png"/>
      <button className="font-bold text-white" onClick={handleSignout}>(SignOut)</button>
-     </div>
+     </div>  } 
       
     </div>
   )
