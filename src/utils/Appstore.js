aa1fc6893e6f39c76ae526e90e50c0df2128ff09
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-08 11:41:17 +0530

    Added Movie Slice reducer

--- src/utils/Appstore.js
+++ src/utils/Appstore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "../utils/userSlice";
+import { movieReducer } from "./movieSlice";
 const Appstore = configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
 });
 
